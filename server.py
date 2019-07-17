18895e7dc91db9aa76cf3f1708aa59db82297a71
clonesupreme <clonesupreme@gmail.com>
2019-07-17 17:46:24 +0100

    Update server.py
    Removed unnecessary code from server file.

--- server.py
+++ server.py
@@ -1,11 +1,5 @@
 import sys
-#import os
 import sqlite3
-#sys.path.append("./Flask-0.10.1")
-#sys.path.append("./Werkzeug-0.9.6")
-#sys.path.append("./Jinja2-2.7.3")
-#sys.path.append("./MarkupSafe-0.23")
-#sys.path.append("./itsdangerous-0.24")
 from flask import Flask
 from flask import g
 from flask import stream_with_context, request, Response, render_template
@@ -15,7 +9,6 @@ app = Flask(__name__, static_folder='', static_url_path='')
 def root():
     return app.send_static_file('index.html')
 
-#DATABASE = '../Data/VEarth.db'
 DATABASE = 'VEarth.db'
 def get_db():
     db = getattr(g, '_database', None)
@@ -38,8 +31,6 @@ def query_db(query, args=(), one=False):
 
 @app.route("/query")
 def query():
-    #return os.getcwd()
-    #@stream_with_context
     def generate():
         for row in query_db('select * from Establishments where BusinessName=?', [request.args['Search']]):
             yield {
@@ -52,44 +43,6 @@ def query():
 			}
 
     return render_template("Resultpage.html", results=generate())
-
-
-@app.route("/pins.json")
-def get_pins_json():
-	@stream_with_context
-	def generate():
-		# yield '{"type":"GeometryCollection","geometries":['
-		# for pin in query_db("select * from Establishments where Latitude like '50.%' and Longitude like '-4%'"):
-		# 	yield '{"type":"Point","coordinates":[%s,%s]},' % (pin['Latitude'], pin['Longitude'])
-		# yield '{"type":"Point","coordinates":[%s,%s]}' % (pin['Latitude'], pin['Longitude'])
-		# yield ']}'
-
-		yield '{"type": "FeatureCollection","features":['
-		for pin in query_db("select * from Establishments where Latitude like '50.%' and Longitude like '-4%'"):
-		 	yield '{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[%s,%s]}},' % (pin['Latitude'], pin['Longitude'])
-		yield '{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[%s,%s]}}' % (pin['Latitude'], pin['Longitude'])
-		yield ']}'
-		# yield '{"type":"Point","coordinates":[%s,%s]}' % (pin['Latitude'], pin['Longitude'])
-		# yield ']}'
-
-           # "features": [
-           #     {"type": "Feature", "properties": {},
-           #         "geometry": {"type": "Point", "coordinates": [-81, 42]}},
-           #     {"type": "Feature", "properties": {},
-           #         "geometry": {"type": "Point", "coordinates": [-82, 43]}},
-           #     {"type": "Feature", "properties": {},
-           #         "geometry": {"type": "Point", "coordinates": [-80, 41]}},
-           #     {"type": "Feature", "properties": {},
-           #         "geometry": {"type": "Point", "coordinates": [19, -24]}},
-           #     {"type": "Feature", "properties": {},
-           #         "geometry": {"type": "Point", "coordinates": [4, 42]}},
-           #     {"type": "Feature", "properties": {},
-           #         "geometry": {"type": "Point", "coordinates": [32, 35]}},
-           # ]
-	return Response (generate())
-
-	#		yield
-	#return Response(generate())
 
 if __name__ == "__main__":
     app.run(debug=True)
