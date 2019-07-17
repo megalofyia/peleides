05a5dd1672af389f9c6516616a19e1f44cfa6f7c
Angus <24278755+Megalofyia@users.noreply.github.com>
2019-07-17 19:37:48 +0100

    Update server.py
    change port from 5000 to 80

--- server.py
+++ server.py
@@ -46,4 +46,4 @@ def query():
 
 if __name__ == "__main__":
     #app.run(debug=True)
-    app.run(host='0.0.0.0', port='5000', debug=False) #do not enable debug while live!!!. It allows remote code execution.
+    app.run(host='0.0.0.0', port='80', debug=False) #do not enable debug while live!!!. It allows remote code execution.
